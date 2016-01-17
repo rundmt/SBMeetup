7e65c6f9af01fe7397d63e9e663bae1824377144
Derek Tor <derekmuoktor@gmail.com>
2016-01-17 03:00:56 -0800

    everything is kinda working

--- src/components/JoinGame/JoinGameApp.js
+++ src/components/JoinGame/JoinGameApp.js
@@ -4,6 +4,10 @@
  */
 'use strict';
 import JoinGameButton from './JoinGameButton'
+var Parse = require('parse/react-native');
+var ParseReact = require('parse-react/react-native');
+var ParseComponent = ParseReact.Component(React);
+
 import React, {
   AppRegistry,
   Component,
@@ -24,20 +28,36 @@ export default class JoinGameApp extends Component {
   constructor(props){
     super(props);
     this.state = {
-      playerinfo: [
-        {name: 'player1'},
-        {name:'player2'}
-      ],
       gameInfo: this.props.navigator.state.routeStack[this.props.navigator.state.routeStack.length -1].gameInfo,
+      players: this.props.navigator.state.routeStack[this.props.navigator.state.routeStack.length -1].gameInfo.players,
       modalShowing: false,
       newPlayerName: ''
     };
-    console.log(this.props.navigator.state.routeStack[this.props.navigator.state.routeStack.length -1].gameInfo);
+    console.log(this.state.gameInfo);
+    // console.log(this.props.navigator.state.routeStack[this.props.navigator.state.routeStack.length -1].gameInfo);
   }
 
   addPlayer(){
-    var newPlayer = {name: this.state.newPlayerName };
-    this.state.playerinfo.push(newPlayer);
+    var Game = Parse.Object.extend('Games');
+    var query = new Parse.Query(Game);
+    var self = this;
+    var newName = this.state.newPlayerName;
+    this.state.players.push(newName);
+    // this.setState({players: })
+    query.get(this.state.gameInfo.objectId, {
+      success: function(game) {
+        game.add('players', newName);
+        game.increment("numberOfPlayers");
+        game.save();
+        // The object was retrieved successfully.
+      },
+      error: function(object, error) {
+        // The object was not retrieved successfully.
+        // error is a Parse.Error with an error code and message.
+      }
+    });
+    // var newPlayer = {name: this.state.newPlayerName };
+    // this.state.playerinfo.push(newPlayer);
     this.closeModal();
   }
 
@@ -77,8 +97,9 @@ export default class JoinGameApp extends Component {
 
           </View>
           <View style={styles.playerList}>
-            {this.state.playerinfo.map((player, index)=>
-              <Text key={index}>{player.name}</Text>
+            {this.state.players.map((player,index)=>
+              <Text key={index}>{player}</Text>
+
             )}
           </View>
 
